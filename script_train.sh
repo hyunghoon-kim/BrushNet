de798330fb1dd52b03f9285c1ed53bbbf968b81d
khh <khh@draph.ai>
2025-02-05 17:47:19 +0900

    train code update variant, train_dataset_len, wandb log

--- script_train.sh
+++ script_train.sh
@@ -1,42 +1,33 @@
 # !/bin/bash
-accelerate launch --num_processes 1 train_brushnet_sdxl_custom.py \
---pretrained_model_name_or_path stabilityai/stable-diffusion-xl-base-1.0 \
+accelerate launch --num_processes 8 train_brushnet_sdxl_custom.py \
+--pretrained_model_name_or_path RunDiffusion/Juggernaut-XL-v9 \
+--variant fp16 \
 --brushnet_model_name_or_path data/ckpt/segmentation_mask_brushnet_ckpt_sdxl_v1 \
 --output_dir runs/logs/brushnetsdxl_segmentationmask \
 --seed 42 \
 --resolution 1024 \
 --train_batch_size 1 \
---max_train_steps 80 \
---checkpointing_steps 8 \
+--train_dataset_len 487 \
+--max_train_steps 4870 \
+--checkpointing_steps 487 \
 --checkpoints_total_limit 3 \
 --gradient_accumulation_steps 1 \
---learning_rate 1e-9 \
+--learning_rate 1e-5 \
 --train_data_dir data/MyData \
---report_to tensorboard \
---validation_steps 8 \
+--report_to wandb \
+--validation_steps 487 \
 --validation_prompt "asian, female, around 20 years old, black wavy hair, wearing red dress" "asian, female, around 20 years old, black wavy hair, wearing black top and white pants" \
 --validation_image "/workspace/BrushNet/test_samples/img/image_1018.png" "/workspace/BrushNet/test_samples/img/image_2318.png" \
 --validation_mask "/workspace/BrushNet/test_samples/inv_mask/image_1018.png" "/workspace/BrushNet/test_samples/inv_mask/image_2318.png" \
 --num_validation_images 1 \
 --resume_from_checkpoint latest \
---tracker_project_name brushnet
+--tracker_project_name brushnet \
+--wandb_run_name test_encode0
 
-################################################################################
-# --train_data_dir data/BrushData
-# --gradient_checkpointing \
-# --set_grads_to_none \
-# --mixed_precision bf16 \
-# --enable_xformers_memory_efficient_attention \
-# --pretrained_model_name_or_path stabilityai/stable-diffusion-xl-base-1.0 \
-# --output_dir runs/logs/brushnetsdxl_segmentationmask \
-# --train_data_dir data/BrushData \
-# --resolution 1024 \
-# --learning_rate 1e-5 \
-# --train_batch_size 1 \
-# --gradient_accumulation_steps 4 \
-# --tracker_project_name brushnet \
-# --report_to tensorboard \
-# --resume_from_checkpoint latest \
-# --validation_steps 300 \
-# --checkpointing_steps 10000 
-################################################################################
+## pretrained_model_name_or_path
+# RunDiffusion/Juggernaut-XL-v9 # if training this ckpt add argument --variant fp16
+# stabilityai/stable-diffusion-xl-base-1.0
+
+## report_to
+# tensorboard
+# wandb
--- train_brushnet_sdxl_custom.py
+++ train_brushnet_sdxl_custom.py
@@ -78,11 +78,9 @@ def log_validation(vae, unet, brushnet, args, accelerator, weight_dtype, step, i
     else:
         brushnet = BrushNetModel.from_pretrained(args.output_dir, torch_dtype=weight_dtype)
         if args.pretrained_vae_model_name_or_path is not None:
-            vae = AutoencoderKL.from_pretrained(args.pretrained_vae_model_name_or_path, torch_dtype=weight_dtype)
+            vae = AutoencoderKL.from_pretrained(args.pretrained_vae_model_name_or_path, torch_dtype=weight_dtype, variant=args.variant)
         else:
-            vae = AutoencoderKL.from_pretrained(
-                args.pretrained_model_name_or_path, subfolder="vae", torch_dtype=weight_dtype
-            )
+            vae = AutoencoderKL.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae", torch_dtype=weight_dtype, variant=args.variant) # 😀 FIX
 
         pipeline = StableDiffusionXLBrushNetPipeline.from_pretrained(
             args.pretrained_model_name_or_path,
@@ -115,8 +113,8 @@ def log_validation(vae, unet, brushnet, args, accelerator, weight_dtype, step, i
 
     image_logs = []
     # inference_ctx = contextlib.nullcontext() if is_final_validation else torch.autocast("cuda") # FIX! prevent NaN error
-    inference_ctx = contextlib.nullcontext()
-    # print("validation_prompts:", validation_prompts)
+    inference_ctx = contextlib.nullcontext() # 😀 FIX
+    
     for validation_prompt, validation_image, validation_mask in zip(validation_prompts, validation_images, validation_masks):
         validation_image = Image.open(validation_image).convert("RGB")
         validation_image = validation_image.resize((args.resolution, args.resolution))
@@ -587,6 +585,22 @@ def parse_args(input_args=None):
             " more information see https://huggingface.co/docs/accelerate/v0.17.0/en/package_reference/accelerator#accelerate.Accelerator"
         ),
     )
+    # 😀 ADD
+    parser.add_argument(
+        "--wandb_run_name",
+        type=str,
+        default=None,
+        help="Wandb log name"
+    )
+    # 😀 ADD
+    parser.add_argument(
+        "--train_dataset_len",
+        type=int,
+        default=None,
+        required=True,
+        help="Number of training samples to be used for computing training steps (e.g., 5000)."
+    )
+
     parser.add_argument(
         "--random_mask",
         action="store_true",
@@ -635,70 +649,6 @@ def parse_args(input_args=None):
     return args
 
 
-# def get_train_dataset(args, accelerator):
-#     # Get the datasets: you can either provide your own training and evaluation files (see below)
-#     # or specify a Dataset from the hub (the dataset will be downloaded automatically from the datasets Hub).
-
-#     # In distributed training, the load_dataset function guarantees that only one local process can concurrently
-#     # download the dataset.
-#     if args.dataset_name is not None:
-#         # Downloading and loading a dataset from the hub.
-#         dataset = load_dataset(
-#             args.dataset_name,
-#             args.dataset_config_name,
-#             cache_dir=args.cache_dir,
-#         )
-#     else:
-#         if args.train_data_dir is not None:
-#             dataset = load_dataset(
-#                 args.train_data_dir,
-#                 cache_dir=args.cache_dir,
-#             )
-#         # See more about loading custom images at
-#         # https://huggingface.co/docs/datasets/v2.0.0/en/dataset_script
-
-#     # Preprocessing the datasets.
-#     # We need to tokenize inputs and targets.
-#     column_names = dataset["train"].column_names
-
-#     # 6. Get the column names for input/target.
-#     if args.image_column is None:
-#         image_column = column_names[0]
-#         logger.info(f"image column defaulting to {image_column}")
-#     else:
-#         image_column = args.image_column
-#         if image_column not in column_names:
-#             raise ValueError(
-#                 f"`--image_column` value '{args.image_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
-#             )
-
-#     if args.caption_column is None:
-#         caption_column = column_names[1]
-#         logger.info(f"caption column defaulting to {caption_column}")
-#     else:
-#         caption_column = args.caption_column
-#         if caption_column not in column_names:
-#             raise ValueError(
-#                 f"`--caption_column` value '{args.caption_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
-#             )
-
-#     if args.conditioning_image_column is None:
-#         conditioning_image_column = column_names[2]
-#         logger.info(f"conditioning image column defaulting to {conditioning_image_column}")
-#     else:
-#         conditioning_image_column = args.conditioning_image_column
-#         if conditioning_image_column not in column_names:
-#             raise ValueError(
-#                 f"`--conditioning_image_column` value '{args.conditioning_image_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
-#             )
-
-#     with accelerator.main_process_first():
-#         train_dataset = dataset["train"].shuffle(seed=args.seed)
-#         if args.max_train_samples is not None:
-#             train_dataset = train_dataset.select(range(args.max_train_samples))
-#     return train_dataset
-
-
 # Adapted from pipelines.StableDiffusionXLPipeline.encode_prompt
 def encode_prompt(prompt_batch, text_encoders, tokenizers, proportion_empty_prompts, is_train=True):
     prompt_embeds_list = []
@@ -738,63 +688,6 @@ def encode_prompt(prompt_batch, text_encoders, tokenizers, proportion_empty_prom
     prompt_embeds = torch.concat(prompt_embeds_list, dim=-1)
     pooled_prompt_embeds = pooled_prompt_embeds.view(bs_embed, -1)
     return prompt_embeds, pooled_prompt_embeds
-
-
-# def prepare_train_dataset(dataset, accelerator):
-#     image_transforms = transforms.Compose(
-#         [
-#             transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR),
-#             transforms.CenterCrop(args.resolution),
-#             transforms.ToTensor(),
-#             transforms.Normalize([0.5], [0.5]),
-#         ]
-#     )
-
-#     conditioning_image_transforms = transforms.Compose(
-#         [
-#             transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR),
-#             transforms.CenterCrop(args.resolution),
-#             transforms.ToTensor(),
-#         ]
-#     )
-
-#     def preprocess_train(examples):
-#         images = [image.convert("RGB") for image in examples[args.image_column]]
-#         images = [image_transforms(image) for image in images]
-
-#         conditioning_images = [image.convert("RGB") for image in examples[args.conditioning_image_column]]
-#         conditioning_images = [conditioning_image_transforms(image) for image in conditioning_images]
-
-#         examples["pixel_values"] = images
-#         examples["conditioning_pixel_values"] = conditioning_images
-
-#         return examples
-
-#     with accelerator.main_process_first():
-#         dataset = dataset.with_transform(preprocess_train)
-
-#     return dataset
-
-
-# def collate_fn(examples):
-#     pixel_values = torch.stack([example["pixel_values"] for example in examples])
-#     pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-
-#     conditioning_pixel_values = torch.stack([example["conditioning_pixel_values"] for example in examples])
-#     conditioning_pixel_values = conditioning_pixel_values.to(memory_format=torch.contiguous_format).float()
-
-#     prompt_ids = torch.stack([torch.tensor(example["prompt_embeds"]) for example in examples])
-
-#     add_text_embeds = torch.stack([torch.tensor(example["text_embeds"]) for example in examples])
-#     add_time_ids = torch.stack([torch.tensor(example["time_ids"]) for example in examples])
-
-#     return {
-#         "pixel_values": pixel_values,
-#         "conditioning_pixel_values": conditioning_pixel_values,
-#         "prompt_ids": prompt_ids,
-#         "unet_added_conditions": {"text_embeds": add_text_embeds, "time_ids": add_time_ids},
-#     }
-
 
 
 class MyWebDataset():
@@ -921,9 +814,10 @@ class MyWebDataset():
 
             masked_image=image*mask
 
-            if random.random()<0.5:
-                masked_image=image-masked_image
-                mask=1-mask
+            ## 😀 DEL, for generalization
+            # if random.random()<0.5:
+                # masked_image=image-masked_image
+                # mask=1-mask
             
             h,w,c=image.shape
             original_size = (h,w)
@@ -934,6 +828,12 @@ class MyWebDataset():
                 scale=self.resolution/w
             w_new=int(np.ceil(w*scale))
             h_new=int(np.ceil(h*scale))
+
+            ## 😀 ADD, for check
+            # tmp_name = random.randint(0, 20)
+            # cv2.imwrite(f"{tmp_name}_image.jpg", image)
+            # cv2.imwrite(f"{tmp_name}_masked_image.jpg", masked_image)
+            # cv2.imwrite(f"{tmp_name}_mask.jpg", mask*255)
             
             image=cv2.resize(image,(w_new,h_new),interpolation=cv2.INTER_CUBIC)
             masked_image=cv2.resize(masked_image,(w_new,h_new),interpolation=cv2.INTER_CUBIC)
@@ -1218,8 +1118,8 @@ def main(args):
                     data_files={"train": os.path.join(args.train_data_dir,"*.tar")}, 
                     split="train", 
                     streaming=True)
-    # train_dataset_len= 10000*len(os.listdir(args.train_data_dir)) # 😀 convert real sampels cnt
-    train_dataset_len= 8 # 😀 convert real sampels cnt 8
+    # train_dataset_len= 10000*len(os.listdir(args.train_data_dir)) # 😀 REPLACE
+    train_dataset_len = args.train_dataset_len # 😀 REPACE
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         shuffle=False,
@@ -1229,49 +1129,13 @@ def main(args):
     )
     train_dataloader_len=train_dataset_len//args.train_batch_size
 
-    # del text_encoders, tokenizers
-    # gc.collect()
-    # torch.cuda.empty_cache()
-
-    # text_encoders = [text_encoder_one, text_encoder_two]
-    # tokenizers = [tokenizer_one, tokenizer_two]
-    # train_dataset = get_train_dataset(args, accelerator)
-    # compute_embeddings_fn = functools.partial(
-    #     compute_embeddings,
-    #     text_encoders=text_encoders,
-    #     tokenizers=tokenizers,
-    #     proportion_empty_prompts=args.proportion_empty_prompts,
-    # )
-    # with accelerator.main_process_first():
-    #     from datasets.fingerprint import Hasher
-
-    #     # fingerprint used by the cache for the other processes to load the result
-    #     # details: https://github.com/huggingface/diffusers/pull/4038#discussion_r1266078401
-    #     new_fingerprint = Hasher.hash(args)
-    #     train_dataset = train_dataset.map(compute_embeddings_fn, batched=True, new_fingerprint=new_fingerprint)
-
-    # del text_encoders, tokenizers
-    # gc.collect()
-    # torch.cuda.empty_cache()
-
-    # # Then get the training dataset ready to be passed to the dataloader.
-    # train_dataset = prepare_train_dataset(train_dataset, accelerator)
-
-    # train_dataloader = torch.utils.data.DataLoader(
-    #     train_dataset,
-    #     shuffle=True,
-    #     collate_fn=collate_fn,
-    #     batch_size=args.train_batch_size,
-    #     num_workers=args.dataloader_num_workers,
-    # )
-
     # Scheduler and math around the number of training steps.
     overrode_max_train_steps = False
     num_update_steps_per_epoch = math.ceil(train_dataloader_len / args.gradient_accumulation_steps)
     if args.max_train_steps is None:
-        args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
+        args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch # 😀 ASSIGN
         overrode_max_train_steps = True
-
+        
     lr_scheduler = get_scheduler(
         args.lr_scheduler,
         optimizer=optimizer,
@@ -1287,11 +1151,16 @@ def main(args):
     )
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-    num_update_steps_per_epoch = math.ceil(train_dataloader_len / args.gradient_accumulation_steps)
+    # num_update_steps_per_epoch = math.ceil(train_dataloader_len / args.gradient_accumulation_steps) # 😀 REPLACE
+    num_update_steps_per_epoch = math.ceil(train_dataloader_len / (accelerator.num_processes * args.gradient_accumulation_steps)) # 😀 REPLACE
+
     if overrode_max_train_steps:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
     # Afterwards we recalculate our number of training epochs
     args.num_train_epochs = math.ceil(args.max_train_steps / num_update_steps_per_epoch)
+    print(f"😀 num_update_steps_per_epoch={num_update_steps_per_epoch}") if accelerator.is_main_process else ... # 😀 ADD PRINT
+    print(f"😀 num_train_epochs={args.num_train_epochs}") if accelerator.is_main_process else ... # 😀 ADD PRINT
+    print(f"😀 args.max_train_steps={args.max_train_steps}") if accelerator.is_main_process else ... # 😀 ADD PRINT
 
     # We need to initialize the trackers we use, and also store our configuration.
     # The trackers initializes automatically on the main process.
@@ -1302,8 +1171,11 @@ def main(args):
         tracker_config.pop("validation_prompt")
         tracker_config.pop("validation_image")
         tracker_config.pop("validation_mask")
+        init_kwargs = {} # 😀 ADD for wandb
+        if args.wandb_run_name:
+            init_kwargs["wandb"] = {"name": args.wandb_run_name}
 
-        accelerator.init_trackers(args.tracker_project_name, config=tracker_config)
+        accelerator.init_trackers(args.tracker_project_name, config=tracker_config, init_kwargs=init_kwargs)
 
     # Train!
     total_batch_size = args.train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
@@ -1479,7 +1351,7 @@ def main(args):
                         logger.info(f"Saved state to {save_path}")
 
                     if args.validation_prompt is not None and global_step % args.validation_steps == 0:
-                        print("during train! 😀😀😀😀😀😀", flush=True)
+                        print(f"during train validation! 😀 epoch: {epoch+1}, global_step: {global_step}", flush=True)
                         image_logs = log_validation(
                             vae=vae,
                             unet=unet,
@@ -1507,7 +1379,7 @@ def main(args):
         # Setting `vae`, `unet`, and `brushnet` to None to load automatically from `args.output_dir`.
         image_logs = None
         if args.validation_prompt is not None:
-            print("final! 😀😀😀😀😀😀", flush=True)
+            print("final validation! 😀", flush=True)
             image_logs = log_validation(
                 vae=None,
                 unet=None,
